4233480597b4f5bc56db2d803558be0389f57582
Dennis Limache <djlimachec@unjbg.edu.pe>
2025-05-31 07:41:51 -0500

    Ejercicio 2 de funciones, calculando cociente y residuo

---
+++ ejercicio_2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int cociente(int val1, int val2);
+int residuo(int val1, int val2);
+int main(){
+    int num1, num2, aux, coci, resi;
+    cout<<"Ingrese un numero: ";
+    cin>>num1;
+    cout<<"Ingrese otro numero: ";
+    cin>>num2;
+    if(num2>num1){
+        aux=num2;
+        num2=num1;
+        num1=aux;
+    }
+    coci=cociente(num1, num2);
+    resi=residuo(num1, num2);
+    cout<<"Residuo: "<<resi<<endl;
+    cout<<"Cociente: "<<coci<<endl;
+    return 0;
+}
+
+int cociente(int val1, int val2){
+    int c;
+    c=0;
+    while(val1>=val2){
+        val1=val1-val2;
+        c=c+1;
+    }
+    return (c);
+}
+int residuo(int val1, int val2){
+    while(val1>=val2){
+        val1=val1-val2;
+    }
+    return (val1);
+}
